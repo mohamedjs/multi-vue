0c427804d3df0f3929cfac28e44175124a6132a4
yvYJPpZmsu1 <mohamed@ubuntu.ubuntu-domain>
2020-01-31 22:40:45 +0200

    make production

--- database/factories/UserAdressFactory.php
+++ database/factories/UserAdressFactory.php
@@ -4,7 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\UserAddress::class, function (Faker $faker){
     return [
-        'details'  => random_int(1,5),
         'address' => $faker->text(random_int(30,100)),
         'city_id' => App\Models\City::inRandomOrder()->first(),
         'user_id' => App\User::inRandomOrder()->first(),
